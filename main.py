9c9896e15b173a0f62ae9e1c66b38a54d0bf1435
AndreaGaietto <a.gaietto1@gmail.com>
2021-11-07 13:04:28 -0500

    Can now complete the entire process of signing up for a slot or waitlist slot. Date/time hardcoded in - need to refactor to allow for automation.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from configparser import SafeConfigParser
 #used for debugging
 import time
 
@@ -26,12 +27,22 @@ for item in item_list:
     # get the child of this element that lists the time slot
     time_slot = grandparent_element.find_element_by_class_name("GXPTime")
     # check to see if the time slot is the one we want, if it is, grab href and break out of loop
-    if time_slot.text == "1:00PM-1:30PM":
+    if time_slot.text == "4:00PM-4:30PM":
         link_elem = item.find_element_by_xpath("./following-sibling::a[1]")
         link = link_elem.get_attribute('href')
         break
+# go to proper signup page
 driver.get(link)
-
+# get my username and password, input into correct fields and submit
+parser = SafeConfigParser()
+parser.read('config.ini')
+email = parser.get('signup_bot', 'email')
+password = parser.get('signup_bot', 'password')
+driver.find_element_by_id("login").send_keys(email)
+driver.find_element_by_id('password').send_keys(password)
+driver.find_element_by_name('submit').click()
+# click submit to finalize reservation
+driver.find_element_by_name('submit').click()
 
 driver.close()
 driver.quit()
